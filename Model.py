104fc1f5d0049c008f9e13f052bec9eadfdeb550
Luke Olawale <lukeolawale4@gmail.com>
2022-08-12 15:20:04 +0100

    updated model

--- Model.py
+++ Model.py
@@ -1,67 +1,125 @@
+"""
+Artist, Venue and Show models
+"""
+# Imports
 
-# from flask_migrate import Migrate
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import ARRAY
-# db = SQLAlchemy()
-
-# def db_setup(app):
-#     db.app = app
-#     db.init_app(app)
-#     Migrate(app,db)
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import DateTime
-from flask_migrate import Migrate
 
-
-app = Flask(__name__)
 db = SQLAlchemy()
-migrate = Migrate(app, db)
 
-# from app import db
+# Models.
 
+
+class Artist(db.Model):
+    """ Artist Model"""
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(120))
+
+    venues = db.relationship('Venue', secondary='shows')
+    shows = db.relationship('Show', backref=('artists'))
+
+    def to_dict(self):
+        """ Returns a dictinary of artists """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'city': self.city,
+            'state': self.state,
+            'phone': self.phone,
+            'genres': self.genres.split(','),  # convert string to list
+            'image_link': self.image_link,
+            'facebook_link': self.facebook_link,
+            'website_link': self.website_link,
+            'seeking_venue': self.seeking_venue,
+            'seeking_description': self.seeking_description,
+        }
+        
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
 
 
 class Venue(db.Model):
-    __tablename__ = 'venue'
+    """ Venue Model """
+    __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    image_link = db.Column(db.String(500), nullable=False)
-    facebook_link = db.Column(db.String(120), nullable=False)
-    website_link = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500), nullable = False)
-    genres = db.Column(db.JSON, nullable=False, default=[])
-    shows = db.relationship('Show', backref='venue', lazy=False)
+    seeking_description = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    artists = db.relationship('Artist', secondary='shows')
+    shows = db.relationship('Show', backref=('venues'))
 
-class Artist(db.Model):
-    __tablename__ = 'artist'
+    def to_dict(self):
+        """ Returns a dictinary of vevenuesnues """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'city': self.city,
+            'state': self.state,
+            'address': self.address,
+            'phone': self.phone,
+            'genres': self.genres.split(','),  # convert string to list
+            'image_link': self.image_link,
+            'facebook_link': self.facebook_link,
+            'website_link': self.website_link,
+            'seeking_talent': self.seeking_talent,
+            'seeking_description': self.seeking_description,
+        }
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120),  nullable=False)
-    phone = db.Column(db.String(120),  nullable=False)
-    genres = db.Column(db.JSON,  nullable=False, default=[])
-    image_link = db.Column(db.String(500),  nullable=False)
-    facebook_link = db.Column(db.String(120),  nullable=False)
-    website_link = db.Column(db.String(120), nullable=False)
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500), nullable = False)
-    shows = db.relationship('Show', backref='artist', lazy=False)
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
 
 
 class Show(db.Model):
-    __tablename__ = 'show'
+    """ Show Model """
+    __tablename__ = 'shows'
+
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable = False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artists.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'venues.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
 
+    venue = db.relationship('Venue')
+    artist = db.relationship('Artist')
+
+    def show_artist(self):
+        """ Returns a dictinary of artists for the show """
+        return {
+            'artist_id': self.artist_id,
+            'artist_name': self.artist.name,
+            'artist_image_link': self.artist.image_link,
+            # convert datetime to string
+            'start_time': self.start_time.strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+    def show_venue(self):
+        """ Returns a dictinary of venues for the show """
+        return {
+            'venue_id': self.venue_id,
+            'venue_name': self.venue.name,
+            'venue_image_link': self.venue.image_link,
+            # convert datetime to string
+            'start_time': self.start_time.strftime('%Y-%m-%d %H:%M:%S')
+        }
